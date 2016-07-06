47af008672a5a42467f1685357530562e77c737a
Michael Grosse <grosse@cosmocode.de>
2016-07-06 17:00:43 +0200

    Initial commit

---
+++ lang/en/lang.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * English language file for tabbedsearchresults plugin
+ *
+ * @author Michael Große <dokuwiki@cosmocode.de>
+ */
+
+$lang['js']['tab:Page Results'] = 'Page Results';
+
+
+
+//Setup VIM: ex: et ts=4 :
---
+++ script.js
@@ -0,0 +1,39 @@
+jQuery(function () {
+    'use strict';
+    if (!jQuery('div#dokuwiki__top.mode_search').length) {
+        return;
+    }
+
+    var $quickresults = jQuery('.search_quickresult');
+    $quickresults.hide();
+    var $fulltext = jQuery('.search_results');
+    var $tabs = jQuery('<ul class="tabs"></ul>');
+
+    var numResults = $fulltext.find('dt').length;
+    $fulltext = $fulltext.add(jQuery('.nothing'));
+    var $searchResults = jQuery('<li class="active"></li>');
+    var title = LANG['plugins']['tabbedsearchresults']['tab:Page Results'] + ' (' + numResults + ')';
+    $searchResults.append(jQuery('<a>'+title+'</a>').click(function (evt) {
+        $quickresults.hide();
+        $fulltext.show();
+        $tabs.find('>li').removeClass('active');
+        $searchResults.addClass('active');
+    }));
+    $tabs.append($searchResults);
+
+    $quickresults.each(function (index, elem) {
+        var $elem = jQuery(elem);
+        var numResults = $elem.find('ul.search_quickhits > li').length;
+        var title = $elem.find('h3').text();
+        $elem.find('h3').hide();
+        var $li = jQuery('<li></li>').append(jQuery('<a>'+title+ ' (' + numResults + ')</a>').click(function (evt) {
+            $quickresults.hide();
+            $fulltext.hide();
+            $elem.show();
+            $tabs.find('>li').removeClass('active');
+            jQuery(this).closest('li').addClass('active');
+        }));
+        $tabs.append($li);
+    });
+    jQuery('#results').after($tabs);
+});
